1edbccba425d81f4853e1cc577bced0da6213d19
* <gsbenevides2@gmail.com>
2020-01-23 20:10:34 -0300

    feat:lrc file upload

--- web/scripts/lyricsScreen.js
+++ web/scripts/lyricsScreen.js
@@ -13,7 +13,22 @@ const lyricsScreen = new Vue({
 	 musicData.url = urlScreen.url
 	 this.visible = false
 	 loadingScreen.downloadStart(musicData)
-	 	}
+	},
+	link(){
+	 const fileInput = document.createElement("input")
+	 fileInput.addEventListener("change",async (e)=>{
+		const file = fileInput.files[0]
+		const musicData =albumScreen.music
+		musicData.lyrics = await file.text()
+		musicData.translation = null
+		musicData.url = urlScreen.url
+		this.visible = false
+		loadingScreen.downloadStart(musicData)
+
+	 })
+	 fileInput.setAttribute("type","file")
+	 fileInput.click()
+	}
  },
  template:`
  <div v-show="visible" class="screen">
@@ -24,6 +39,7 @@ const lyricsScreen = new Vue({
  <button @click="next(true,true)" class="mdc-button" style="width:100%" v-if="translation">Continuar com letra e tradução</button>
  <button @click="next(false,true)" class="mdc-button" style="width:100%">Continuar somente com a letra</button>
  <button @click="next(false,false)" class="mdc-button" style="width:100%">Continuar sem a letra</button>
+ <button @click="link()" class="mdc-button" style="width:100%">LRC</button>
  </div>
  `
 })
