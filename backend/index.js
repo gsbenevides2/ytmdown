37fdcd7c864306e08826316b5f0665f381d97dae
gsbenevides2 <gsbenevides2@gmail.com>
2020-02-26 13:39:39 -0300

    fix:cors in web.app

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const whitelist = [
 'http://localhost:3000',
 'http://localhost:5000',
-'https://ytmdown.web.app/',
+'https://ytmdown.web.app',
 'https://ytmdown.firebaseapp.com',
 'https://ytmdown.herokuapp.com',
  undefined
