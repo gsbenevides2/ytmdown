828caa67850ad7bc014c43188d4355a0ba102303
gsbenevides2 <gsbenevides2@gmail.com>
2020-03-10 20:03:58 -0300

    test:improve pwa

--- webDeploy.js
+++ webDeploy.js
@@ -1,6 +1,31 @@
+const {writeFileSync,readFileSync} = require('fs')
+
 //This script changes the public folder to the folder with the mimicked files.
-const {writeFileSync} = require('fs')
 const firebaseJson = require('./firebase.json')
 firebaseJson.hosting.public = 'web/public'
 writeFileSync('./firebase.json',JSON.stringify(firebaseJson,null,4))
 
+//This script generate new sw from sw.template.js
+const {getFilesList} = require('minify-cli/utils')
+const version = require('./package.json').webVersion
+const template = readFileSync('./sw.template').toString()
+getFilesList('/web/src').then(fileList=>{
+ const htmls =fileList.html.map(html=>{
+	if(html!== 'index.html'){
+	 return html.slice(0,-5)
+	}
+	else return '/'
+ })
+ const files = [...fileList.css,...fileList.js,...htmls]
+ let string = '\n'
+ for(file of files){
+	const fileObject = {
+	 url:file,
+	 revision:version
+	}
+	string += JSON.stringify(fileObject) + ",\n"
+ }
+ const final = template.replace(/<files>/,string.slice(0,-2))
+ console.log('ok')
+ writeFileSync('./web/public/sw.js',final)
+})
--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
- "globDirectory": "web/public/",
-  "globPatterns": [
-    "**/*.{ico,png,html,css,js,json}"
-  ],
- "swDest": "web/public/sw.js"
-};
