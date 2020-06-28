484f8792b228ff7dd2724d35fc08d1f3828ec929
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2020-06-28 17:10:46 -0300

    simplified firebase environment variables

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -2,18 +2,7 @@ import * as firebase from 'firebase/app'
 import 'firebase/performance'
 import 'firebase/analytics'
 
-const env = process.env
-const firebaseConfig = {
-  apiKey: env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  databaseURL: env.REACT_APP_FIREBASE_DATABASEURL,
-  projectId: env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: env.REACT_APP_FIREBASE_APPID,
-  measurementId: env.REACT_APP_FIREBASE_MEASUREMENTID
-}
-
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE as string)
 firebase.initializeApp(firebaseConfig)
 firebase.performance()
 firebase.analytics()
