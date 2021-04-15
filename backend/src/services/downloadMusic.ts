1c8c226131ebb3b5fa90f5be4f9a393d4fd71a1b
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2021-04-15 01:24:35 +0000

    fix

--- backend/src/services/downloadMusic.ts
+++ backend/src/services/downloadMusic.ts
@@ -136,7 +136,7 @@ function downloadCompleteMusic (data: MusicData, id: string, callback: CallbackT
         artist: data.artist,
         album: data.album,
         APIC: coverFilePath,
-        year: String(data.year),
+        year: data.year,
         trackNumber: String(data.number)
       }
       if (data.lyrics || data.translation) {
